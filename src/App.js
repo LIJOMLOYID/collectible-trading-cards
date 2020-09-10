7f274bca10ee7e2adfe19c52fdb4558c769fb829
LIJOMLOYID <lijomloyid@gmail.com>
2020-09-11 02:35:02 +0530

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,7 +5,20 @@ import './App.css';
 class App extends React.Component {
     render() {
     	return (
-    		<h1> Card Placeholder </h1>
+    		<div className="container">
+    			<div className="row">
+    				<div className="col-md-4">
+    					<div className="card" >
+						  <img className="card-img-top" src="..." alt="Card image cap"/>
+						  <div className="card-body">
+						    <h5 className="card-title">Card title</h5>
+						    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						    <a href="#" className="btn btn-primary">Go somewhere</a>
+						  </div>
+						</div>
+    				</div>
+    			</div>
+    		</div>
     	);
 	}
 }
