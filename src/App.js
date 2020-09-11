aaf251b3184293dba72dd0758cbfe6b620afabc6
LIJOMLOYID <lijomloyid@gmail.com>
2020-09-11 15:14:52 +0530

    Added info cards

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
     			<div className="row">
     				<div className="col-md-4">
     					<div className="card" >
-						  <img className="card-img-top" src="..." alt="Card image cap"/>
+						  <img className="card-img-top" src="https://api.adorable.io/avatars/285/abott@adorable.png" alt="Card image cap"/>
 						  <div className="card-body">
 						    <h5 className="card-title">Card title</h5>
 						    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
