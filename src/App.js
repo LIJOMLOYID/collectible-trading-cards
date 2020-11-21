c49b1705b6c5259be34ba283cbb2291ece091cef
LIJOMLOYID <lijomloyid@gmail.com>
2020-11-21 18:34:07 +0530

    Customized cards, added name and other details

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import Card from './components/card'
+import Card from "./components/card";
 class App extends React.Component {
-    render() {
-    	return (
-    		<div className="container">
-    			<div className="row">
-    				<Card/>
-    				<Card/>
-    				<Card/>
-    				<Card/>
-    				<Card/>
-    				<Card/>
-    			</div>
-    		</div>
-    	);
-	}
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <Card name="Lijo M Loyid" job="Software Engineer" avatarId={52} />
+          <Card name="James Smith" job="Pilot" avatarId={51} />
+          <Card name="Jessica Jones" job="Nurse" avatarId={28} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/card.js
+++ src/components/card.js
@@ -1,18 +1,24 @@
-import React from 'react'
+import React from "react";
 
-const Card = () => {
-	return(
-		<div className='col-md-4'>
-				<div className="card" >
-				  <img className="card-img-top" src="https://api.adorable.io/avatars/285/abott@adorable.png" alt="Card image cap"/>
-				  <div className="card-body">
-				    <h5 className="card-title">Card title</h5>
-				    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-				    <a href="#" className="btn btn-primary">Go somewhere</a>
-				  </div>
-				</div>
-		</div>
-	);
-}
+const Card = (props) => {
+  const avatar = `https://i.pravatar.cc/150?img=${props.avatarId}`;
+  return (
+    <div className="col-md-4">
+      <div className="card">
+        <img className="card-img-top" src={avatar} alt="Card image cap" />
+        <div className="card-body">
+          <h5 className="card-title">{props.name}</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="#" className="btn btn-primary">
+            {props.job}
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Card;
