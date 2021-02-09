f3e6eb6e168958cd652d565dace436b894b55926
LIJOMLOYID <lijomloyid@gmail.com>
2021-02-09 15:04:39 +0530

    Added state. Added a button to change the Avatar

--- src/App.js
+++ src/App.js
@@ -2,14 +2,37 @@ import React from "react";
 import "./App.css";
 
 import Card from "./components/card";
+
 class App extends React.Component {
+  state = {
+    avatarId: 28,
+  };
+
+  changeAvatar = () => {
+    this.setState({
+      avatarId: 49,
+    });
+    console.log(this.state);
+  };
+
   render() {
     return (
       <div className="container">
         <div className="row">
           <Card name="Lijo M Loyid" job="Software Engineer" avatarId={52} />
           <Card name="James Smith" job="Pilot" avatarId={51} />
-          <Card name="Jessica Jones" job="Nurse" avatarId={28} />
+          <Card
+            name="Jessica Jones"
+            job="Nurse"
+            avatarId={this.state.avatarId}
+          />
+        </div>
+        <div className="row">
+          <div className="col-md-3">
+            <a onClick={this.changeAvatar} className="changeAvatar" href="#">
+              Change Avatar
+            </a>
+          </div>
         </div>
       </div>
     );
